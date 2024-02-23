f01a7cb3af5222b1a8aafb33e8a6be01d66170c0
hoang long <lainguyenhoanglong.mxt@mgial.com>
2024-02-23 21:26:59 +0700

    first commit

---
+++ console.py
@@ -0,0 +1,9 @@
+name = "name"
+print (type(name))
+print (name*3)
+
+age=1
+print(name + str(age))
+
+username = input("ten b la gi")
+print("u r " + username)
---
+++ veconrua.py
@@ -0,0 +1,60 @@
+
+from turtle import *
+
+
+# square
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+
+# pen
+pensize(10)
+color("aqua")
+shape(name="circle")
+
+# circle
+up()
+setposition(0, -150)
+down()
+color("red")
+circle(radius=20)
+
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+
+left(54 + 18)
+backward(100)
+right(108)
+forward(100)
+left(54 + 18)
+forward(100)
+left(54 + 18)
+forward(100)
+left(54 + 18)
+forward(100)
+
+left(180-36)
+forward(100)
+left(180-36)
+forward(100)
+left(180-36)
+forward(100)
+left(180-36)
+forward(100)
+left(180-36)
+forward(100)
+
+mainloop()
